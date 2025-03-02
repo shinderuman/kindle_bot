8aff8a5b1c604e2a2180b863d0f2d82cf007276f
shinderuman <shinderuman@gmail.com>
2025-03-03 04:56:50 +0900

    Initialize Kindle bot with ASIN search and sale detection logic
    - Added `.gitignore` for local development artifacts
    - Implemented `asin_search.go` to search Kindle versions for paper books and notify via Slack
    - Implemented `sale.go` to detect sales and post to Mastodon/Slack when price drops or high points are available
    - Added `utils/` package for configuration loading, S3 storage handling, Slack/Mastodon/Gist integrations
    - Added Go modules with required dependencies

---
+++ utils/models.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"github.com/goark/pa-api/entity"
+)
+
+type Config struct {
+	S3BucketName           string `json:"S3BucketName"`
+	S3UnprocessedObjectKey string `json:"S3UnprocessedObjectKey"`
+	S3PaperBooksObjectKey  string `json:"S3PaperBooksObjectKey"`
+	S3Region               string `json:"S3Region"`
+	AmazonPartnerTag       string `json:"AmazonPartnerTag"`
+	AmazonAccessKey        string `json:"AmazonAccessKey"`
+	AmazonSecretKey        string `json:"AmazonSecretKey"`
+	MastodonServer         string `json:"MastodonServer"`
+	MastodonClientID       string `json:"MastodonClientID"`
+	MastodonClientSecret   string `json:"MastodonClientSecret"`
+	MastodonAccessToken    string `json:"MastodonAccessToken"`
+	SlackBotToken          string `json:"SlackBotToken"`
+	SlackNoticeChannel     string `json:"SlackNoticeChannel"`
+	SlackErrorChannel      string `json:"SlackErrorChannel"`
+	GistID                 string `json:"GistID"`
+	GitHubToken            string `json:"GitHubToken"`
+}
+
+type KindleBook struct {
+	ASIN         string      `json:"ASIN"`
+	Title        string      `json:"Title"`
+	ReleaseDate  entity.Date `json:"ReleaseDate"`
+	CurrentPrice float64     `json:"CurrentPrice"`
+	MaxPrice     float64     `json:"MaxPrice"`
+	URL          string      `json:"URL"`
+}
---
+++ utils/utils.go
@@ -0,0 +1,313 @@
+package utils
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"runtime"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	paapi5 "github.com/goark/pa-api"
+	"github.com/goark/pa-api/entity"
+	"github.com/goark/pa-api/query"
+	"github.com/mattn/go-mastodon"
+	"github.com/slack-go/slack"
+)
+
+var EnvConfig Config
+
+func InitConfig() error {
+	if IsLambda() {
+		EnvConfig = Config{
+			S3BucketName:           os.Getenv("S3_BUCKET_NAME"),
+			S3UnprocessedObjectKey: os.Getenv("S3_UNPROCESSED_OBJECT_KEY"),
+			S3PaperBooksObjectKey:  os.Getenv("S3_PAPER_BOOKS_OBJECT_KEY"),
+			S3Region:               os.Getenv("S3_REGION"),
+			AmazonPartnerTag:       os.Getenv("AMAZON_PARTNER_TAG"),
+			AmazonAccessKey:        os.Getenv("AMAZON_ACCESS_KEY"),
+			AmazonSecretKey:        os.Getenv("AMAZON_SECRET_KEY"),
+			MastodonServer:         os.Getenv("MASTODON_SERVER"),
+			MastodonClientID:       os.Getenv("MASTODON_CLIENT_ID"),
+			MastodonClientSecret:   os.Getenv("MASTODON_CLIENT_SECRETT"),
+			MastodonAccessToken:    os.Getenv("MASTODON_ACCESS_TOKEN"),
+			SlackBotToken:          os.Getenv("SLACK_BOT_TOKEN"),
+			SlackNoticeChannel:     os.Getenv("SLACK_NOTICE_CHANNEL"),
+			SlackErrorChannel:      os.Getenv("SLACK_ERROR_CHANNEL"),
+			GistID:                 os.Getenv("GIST_ID"),
+			GitHubToken:            os.Getenv("GITHUB_TOKEN"),
+		}
+	} else {
+		data, err := ioutil.ReadFile("config.json")
+		if err != nil {
+			return err
+		}
+
+		if err := json.Unmarshal(data, &EnvConfig); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func IsLambda() bool {
+	return os.Getenv("AWS_LAMBDA_FUNCTION_NAME") != ""
+}
+
+func Handler(ctx context.Context, callback func() error) (string, error) {
+	if err := callback(); err != nil {
+		log.Println(err)
+		AlertToSlack(err, true)
+		return "", err
+	}
+	_, file, _, _ := runtime.Caller(1)
+	return fmt.Sprintf("Processing complete: %s", file), nil
+}
+
+func FetchASINs(sess *session.Session, objectKey string) ([]KindleBook, error) {
+	svc := s3.New(sess)
+
+	resp, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(EnvConfig.S3BucketName),
+		Key:    aws.String(objectKey),
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var ASINs []KindleBook
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(body, &ASINs); err != nil {
+		return nil, err
+	}
+
+	return ASINs, nil
+}
+
+func UniqueASINs(slice []KindleBook) []string {
+	seen := make(map[string]struct{})
+	result := []string{}
+
+	for _, s := range slice {
+		if _, exists := seen[s.ASIN]; !exists {
+			seen[s.ASIN] = struct{}{}
+			result = append(result, s.ASIN)
+		}
+	}
+
+	return result
+}
+
+func ChunkedASINs(slice []string, size int) [][]string {
+	var chunks [][]string
+	for i := 0; i < len(slice); i += size {
+		end := i + size
+		if end > len(slice) {
+			end = len(slice)
+		}
+		chunks = append(chunks, slice[i:end])
+	}
+	return chunks
+}
+
+func SortByReleaseDate(books []KindleBook) {
+	sort.Slice(books, func(i, j int) bool {
+		if books[i].ReleaseDate.Time.After(books[j].ReleaseDate.Time) {
+			return true
+		} else if books[i].ReleaseDate.Time.Equal(books[j].ReleaseDate.Time) {
+			// ReleaseDate が同じ場合は Title で比較
+			return books[i].Title < books[j].Title
+		}
+		return false
+	})
+}
+
+func GetItems(client paapi5.Client, asinChunk []string) (*entity.Response, error) {
+	q := query.NewGetItems(client.Marketplace(), client.PartnerTag(), client.PartnerType()).
+		ASINs(asinChunk).
+		EnableItemInfo().
+		EnableOffers()
+
+	body, err := requestWithBackoff(client, q)
+	if err != nil {
+		return nil, fmt.Errorf("PA API request failed: %w", err)
+	}
+
+	res, err := entity.DecodeResponse(body)
+	if err != nil {
+		return nil, fmt.Errorf("JSON decode error: %w", err)
+	}
+
+	return res, nil
+}
+
+func SearchItems(client paapi5.Client, q *query.SearchItems) (*entity.Response, error) {
+	body, err := requestWithBackoff(client, q)
+	if err != nil {
+		return nil, fmt.Errorf("PA API request failed: %w", err)
+	}
+
+	res, err := entity.DecodeResponse(body)
+	if err != nil {
+		return nil, fmt.Errorf("JSON decode error: %w", err)
+	}
+
+	return res, nil
+}
+
+func PrintPrettyJSON(v any) {
+	prettyJSON, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return
+	}
+
+	fmt.Println(strings.ReplaceAll(string(prettyJSON), `\u0026`, "&"))
+}
+
+func requestWithBackoff[T paapi5.Query](client paapi5.Client, q T) ([]byte, error) {
+	maxRetries := 5
+
+	for i := 0; i < maxRetries; i++ {
+		body, err := client.Request(q)
+		if err == nil {
+			return body, nil
+		}
+
+		if isRateLimitError(err) {
+			waitTime := time.Duration(math.Pow(2, float64(i))) * time.Second
+			waitTime += time.Duration(rand.Intn(1000)) * time.Millisecond
+			log.Printf("Rate limit hit. Retrying in %v...\n", waitTime)
+			time.Sleep(waitTime)
+			continue
+		}
+
+		return nil, err
+	}
+
+	return nil, fmt.Errorf("Max retries reached")
+}
+
+func isRateLimitError(err error) bool {
+	return strings.Contains(err.Error(), "bad HTTP status: status 429")
+}
+
+func SaveASINs(sess *session.Session, ASINs []KindleBook, objectKey string) error {
+	svc := s3.New(sess)
+
+	prettyJSON, err := json.MarshalIndent(ASINs, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, err = svc.PutObject(&s3.PutObjectInput{
+		Bucket:      aws.String(EnvConfig.S3BucketName),
+		Key:         aws.String(objectKey),
+		Body:        aws.ReadSeekCloser(strings.NewReader(strings.ReplaceAll(string(prettyJSON), `\u0026`, "&"))),
+		ACL:         aws.String("private"),
+		ContentType: aws.String("application/json"),
+	})
+	return err
+}
+
+func AlertToSlack(err error, withMention bool) error {
+	log.Println(err)
+	if withMention {
+		return PostToSlack(fmt.Sprintf("<@U0MHY7ATX>\n```%v```", err), EnvConfig.SlackErrorChannel)
+	} else {
+		return PostToSlack(fmt.Sprintf("```%v```", err), EnvConfig.SlackErrorChannel)
+	}
+}
+
+func PostToSlack(message string, channel ...string) error {
+	api := slack.New(EnvConfig.SlackBotToken)
+
+	targetChannel := EnvConfig.SlackNoticeChannel
+	if len(channel) > 0 {
+		targetChannel = channel[0]
+	}
+
+	_, _, err := api.PostMessage(
+		targetChannel,
+		slack.MsgOptionText(message, false),
+	)
+	return err
+}
+
+func TootMastodon(message string) (*mastodon.Status, error) {
+	c := mastodon.NewClient(&mastodon.Config{
+		EnvConfig.MastodonServer,
+		EnvConfig.MastodonClientID,
+		EnvConfig.MastodonClientSecret,
+		EnvConfig.MastodonAccessToken,
+	})
+
+	return c.PostStatus(context.Background(), &mastodon.Toot{Status: message, Visibility: "public"})
+}
+
+func UpdateGist(books []KindleBook) error {
+	url := fmt.Sprintf("https://api.github.com/gists/%s", EnvConfig.GistID)
+
+	var lines []string
+	for _, book := range books {
+		lines = append(lines, fmt.Sprintf("* [[%s]%s](%s)", book.ReleaseDate.Format("2006-01-02"), book.Title, book.URL))
+	}
+
+	markdown := fmt.Sprintf("## 合計 %d冊\n%s", len(books), strings.Join(lines, "\n"))
+
+	payload := map[string]interface{}{
+		"files": map[string]interface{}{
+			"わいのセールになってほしい本.md": map[string]string{
+				"content": markdown,
+			},
+		},
+	}
+
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("PATCH", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", "token "+EnvConfig.GitHubToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	var client http.Client
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func GetBook(ASIN string, slice []KindleBook) KindleBook {
+	for _, s := range slice {
+		if ASIN == s.ASIN {
+			return s
+		}
+	}
+	return KindleBook{}
+}
